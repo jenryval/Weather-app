ff6cfc024e31b4ed55b8e625fbccf75b40b5c636
jenry valdes <jenryval93@gmail.com>
2021-04-07 19:20:40 -0500

    added Index html foundation

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-var api = "c87dbd6058cc5e50e8f69c46a3447ae6"
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?"
+// var for API keys and URL
+var apiKey = "dc2e669487d07ea7849905a5a053b159";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 
 var searchBox = document.querySelector(".search-box")
 
+// event lisener for enter button
 searchBox.addEventListener("keypress" , submit)
-
 function submit(event){ 
     if (event.keyCode == 13) {
-        // console.log(searchBox.value)
-        getResults(searchBox.value)
+        console.log(searchBox.value)
+        // getResults(searchBox.value)
     }
 }
 
-function getResults(){
-  
+
+function getResults(query){
+fetch(queryURL + query + "&units=metric&appid" + apiKey)
+  .then(weather =>{
+      return weather.json();
+  }).then(display);
+}
+
+function display(weather){
+    console.log(weather)
 }
